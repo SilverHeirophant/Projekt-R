6bec92c72aa71a78566fad5f3ab93c9b8743b604
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2023-11-02 13:55:16 -0400

    Basic script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float verticalInput;
+    public int speed;
+    
+    private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
