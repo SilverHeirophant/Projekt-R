ba662f0810cfea6f5b2665d84c0675c1aaab7654
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2023-11-02 14:09:15 -0400

    ghdsuhgvadfks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Public Variables
     public float horizontalInput;
     public float verticalInput;
     public int speed;
-    
+    public int maxHealth;
+    public int currentHealth;
+    public float timeInvincible = 2.0f;
+
+    //Private Variables
     private Rigidbody playerRb;
+    private float boost;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        currentHealth = maxHealth;
     }
+
+    if(Input.GetButtonDown("Jump"))
+    {
+        speed = speed + boost;
+        
+        if(speed > 15)
+        {
+            boost = false;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
